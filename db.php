15a0fffa482029d896577b24359ac2607c690088
Medvedevahelena <94795347+Medvedevahelena@users.noreply.github.com>
2021-12-04 18:17:59 +0300

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ Website: http://www.allphptricks.com/
 */
 
 
-$con = mysqli_connect("localhost","root","db123","register");
+$con = mysqli_connect("192.168.81.130","root","db123","register");
 // Check connection
 if (mysqli_connect_errno())
   {
